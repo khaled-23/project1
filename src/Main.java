78108374d5def72933bb1d82c4a354684960644f
khaled alkuhaily <khaledalkuhaily@gmail.com>
2024-02-22 18:51:00 +0300

    created gameStart(), added necessary variable, coded switch case to fill the positionArray[][]

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -18,6 +19,7 @@ public class Main {
     public static void gameStart(){
         Scanner s = new Scanner(System.in);
         Scanner s2 = new Scanner(System.in);
+        Scanner s3 = new Scanner(System.in);
 
         int gameMode = 0;
 
@@ -51,19 +53,20 @@ public class Main {
                 boolean isPlayerTurn = true;
                 char playerRole = 'x';
                 char computerRole = 'o';
+                int positionInput=0;
                 boolean winCondtion = false;
-                boolean isAvailable = true;
-
+                boolean isAvailable = false;
+                int count = 0;
 
                 String choice = "not null";
 
-                while(!choice.equalsIgnoreCase("X") || !choice.equalsIgnoreCase("o")){
+                while(!choice.equalsIgnoreCase("x") && !choice.equalsIgnoreCase("o")){
                     System.out.println("please choice X or O");
                     choice = s2.nextLine();
                 }
 
                 //if player choose o, assign o to player and x to computer.
-                if (choice.equalsIgnoreCase("O")){
+                if (choice.equalsIgnoreCase("o")){
                     isPlayerTurn = false;
                     playerRole = 'o';
                     computerRole = 'x';
@@ -71,14 +74,9 @@ public class Main {
 
 
                 do{
-                    //game Board
 
-//                    System.out.println("-----|-----|-----");
-//                    System.out.println("  1  |  2  |  3  ");
-//                    System.out.println("-----|-----|-----");
-//                    System.out.println("  4  |  5  |  6  ");
-//                    System.out.println("-----|-----|-----");
-//                    System.out.println("  7  |  8  |  9  ");
+
+                    //game Board
                     System.out.println("|-----|-----|-----|");
                     System.out.println("|  " + match[0][0] + "  |  " + match[0][1] + "  |  " + match[0][2] + "  |");
                     System.out.println("|-----|-----|-----|");
@@ -88,19 +86,24 @@ public class Main {
                     System.out.println("|-----|-----|-----|");
 
 
+                    do{
+                        System.out.println("computer choose: " + positionInput);
+                        if(isPlayerTurn){
+                            System.out.println("enter an available position");
+                            positionInput = Integer.parseInt(String.valueOf(s.nextInt()));
+                        }else{
+                            positionInput = computerTurn(match);
+                        }
+                        isAvailable = isAvailable(match, positionInput);
 
-                    isAvailable = isAvailable(match);
-                    int positionInput=0;
-                    if(isPlayerTurn){
-                        System.out.println("enter an available position");
-                        positionInput = Integer.parseInt(String.valueOf(s.nextInt()));
-                    }else positionInput = computerTurn(match);
+                    }while(!isAvailable);
 
 
-                    isPlayerTurn = !isPlayerTurn;
-
-                    winCondtion = winCondtion(match);
-
+//                    isAvailable = isAvailable(match, positionInput);
+//
+//
+//
+//                    winCondtion = winCondtion(match);
 
 
                     switch(positionInput){
@@ -112,54 +115,57 @@ public class Main {
 
                         case 2:
                             if(isPlayerTurn) {
-                                match[0][1] = 'X';
-                            }else match[0][1] ='O';
+                                match[0][1] = playerRole;
+                            }else match[0][1] = computerRole;
 
                             break;
                         case 3:
                             if(isPlayerTurn) {
-                                match[0][2] = 'X';
-                            }else match[0][2] ='O';
+                                match[0][2] = playerRole;
+                            }else match[0][2] = computerRole;
 
                             break;
                         case 4:
                             if(isPlayerTurn) {
-                                match[1][0] = 'X';
-                            }else match[1][0] ='O';
+                                match[1][0] = playerRole;
+                            }else match[1][0] = computerRole;
 
                             break;
                         case 5:
                             if(isPlayerTurn) {
-                                match[1][1] = 'X';
-                            }else match[1][1] ='O';
+                                match[1][1] = playerRole;
+                            }else match[1][1] = computerRole;
 
                             break;
                         case 6:
                             if(isPlayerTurn) {
-                                match[1][2] = 'X';
-                            }else match[1][2] ='O';
+                                match[1][2] = playerRole;
+                            }else match[1][2] = computerRole;
 
                             break;
                         case 7:
                             if(isPlayerTurn) {
-                                match[2][0] = 'X';
-                            }else match[2][0] ='O';
+                                match[2][0] = playerRole;
+                            }else match[2][0] = computerRole;
 
                             break;
                         case 8:
                             if(isPlayerTurn) {
-                                match[2][1] = 'X';
-                            }else match[2][1] ='O';
+                                match[2][1] = playerRole;
+                            }else match[2][1] = computerRole;
 
                             break;
                         case 9:
                             if(isPlayerTurn) {
-                                match[2][2] = 'X';
-                            }else match[2][2] ='O';
+                                match[2][2] = playerRole;
+                            }else match[2][2] = computerRole;
                             break;
                     }
 
-                }while(winCondtion || !isAvailable);
+                    winCondtion = winCondtion(match);//check if the match win or draw
+                    isPlayerTurn = !isPlayerTurn;
+
+                }while(!winCondtion);
 
                 break;
             case 2:
@@ -176,23 +182,90 @@ public class Main {
 
     }
 
-    public static boolean isAvailable(char[][] match){
+    public static boolean isAvailable(char[][] match ,int positionInput){
+        boolean isAvailable = true;
 
-        for(int i = 0; i<3; i++){
-            for(int j = 0; j<3; j++){
+        switch(positionInput){
+            case 1:
+                if(match[0][0] == 'x' || match[0][0] == 'o')
+                    isAvailable = false;
+                break;
+            case 2:
+                if(match[0][1] == 'x' || match[0][1] == 'o')
+                    isAvailable = false;
+                break;
+            case 3:
+                if(match[0][2] == 'x' || match[0][2] == 'o')
+                    isAvailable = false;
+                break;
+            case 4:
+                if(match[1][0] == 'x' || match[1][0] == 'o')
+                    isAvailable = false;
+                break;
+            case 5:
+                if(match[1][1] == 'x' || match[1][1] == 'o')
+                    isAvailable = false;
+                break;
+                case 6:
+                if(match[1][2] == 'x' || match[1][2] == 'o')
+                    isAvailable = false;
+                break;
+            case 7:
+                if(match[2][0] == 'x' || match[2][0] == 'o')
+                    isAvailable = false;
+                break;
+            case 8:
+                if(match[2][1] == 'x' || match[2][1] == 'o')
+                    isAvailable = false;
+                break;
+            case 9:
+                if(match[2][2] == 'x' || match[2][2] == 'o')
+                    isAvailable = false;
+                break;
 
-            }
         }
-        return true;
+
+
+
+        return isAvailable;
     }
 
     public static boolean winCondtion(char[][] match){
-        return true;
+
+        boolean firstLineH = (match[0][0] == 'x' && match[0][1] == 'x' && match[0][2] == 'x')
+                || (match[0][0] == 'o' && match[0][1] == 'o' && match[0][2] == 'o');
+        boolean secondLineH = (match[1][0] == 'x' && match[1][1] == 'x' && match[1][2] == 'x')
+                || (match[1][0] == 'o' && match[1][1] == 'o' && match[1][2] == 'o');
+        boolean thirdLineH = (match[2][0] == 'x' && match[2][1] == 'x' && match[2][2] == 'x')
+                || (match[2][0] == 'o' && match[2][1] == 'o' && match[2][2] == 'o');
+        boolean winCondition = false;
+
+        if(firstLineH){
+            System.out.println("win");
+            winCondition = true;
+        }else if(secondLineH){
+            System.out.println("win");
+            winCondition = true;
+        } else if(thirdLineH) {
+            System.out.println("win");
+            winCondition = true;
+        }
+
+        return winCondition;
     }
 
     public static int computerTurn(char[][] match){
 
+        boolean isAvailable = false;
         int computerInput = 0;
+        do{
+            Random rand = new Random();
+            computerInput = rand.nextInt(1,10);
+
+            isAvailable = isAvailable(match, computerInput);
+
+        }while(!isAvailable);
+
 
         return computerInput;
     }
